289289f3f8c42e1f2add6bcb8c421ec8a7253395
Solly <solobarine@gmail.com>
2025-09-16 12:02:26 +0100

    feat(types) - add Message interface

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -2,3 +2,12 @@ export interface Conversation {
   id: string;
   createdAt: string;
 }
+
+export interface Message {
+  id: string;
+  text: string;
+  sender: "USER" | "BOT";
+  conversationId: string;
+  conversation?: Conversation;
+  createdAt: string;
+}
